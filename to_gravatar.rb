5cfa8867168cc6a4c60dd43d6222536e8f741e00
Laurent Desgrange <laurent@desgrange.net>
2012-06-23 10:00:59 +0200

    Added secure URL.

--- to_gravatar.rb
+++ to_gravatar.rb
@@ -3,9 +3,18 @@ require 'digest/md5'
 module Jekyll
   module GravatarFilter
     def to_gravatar(input)
-      email_address = input.downcase.strip
-      hash = Digest::MD5.hexdigest(email_address)
-      "http://www.gravatar.com/avatar/#{hash}"
+      "http://www.gravatar.com/avatar/#{hash(input)}"
+    end
+
+    def to_secure_gravatar(input)
+      "https://secure.gravatar.com/avatar/#{hash(input)}"
+    end
+
+    private :hash
+
+    def hash(email)
+      email_address = email.downcase.strip
+      Digest::MD5.hexdigest(email_address)
     end
   end
 end
